14d34bbbe441428f1ee328dfd494c4a18c5f2a68
Kevin Brennan <82340612+Kevin-B-UTP@users.noreply.github.com>
2021-09-11 17:26:08 -0500

    Update newraph.m

--- newraph.m
+++ newraph.m
@@ -1,26 +1,43 @@
-## newton-raphson(función, derivada, x_0, 0.0001)
+% Implementacion del metodo de newton raphson
+% Grupo: Kevin Brennan, Edwar Gonzalez, Guillermo Lezcano
+% Escrito por Kevin Brennan - 2021
 
-function xn = reglafalsa (funcion, derivada, x_0 , tol)
+## newtonraphson(función, aproximacion=a, 0.0001)
+
+function xn = newtonraphson (funcion, a, tol)
       clc;
-      ciclo = 0; 
       error = 10000;
+      n=0;
       ## ENCABEZADO
       fprintf('\n__________________________________');
       fprintf('\n   MÉTODO DE LA NEWTON-RAPHSON');
       fprintf('\n__________________________________');
-    
       ## INSTRUCCIONES
       
+      f = inline(funcion);
+      derivada_funcion = diff(funcion, x);
+      df = inline (derivada_funcion);
       ## CICLO- EVALUACIÓN- APROXIMACIÓN ?
-      
+      while (er>tol) 
+      fa = feval (f, a);
+      dfa = feval (df, a);
+      xn = a - ((fa)/(dfa));
+        if (n>0)
+        
+          er=abs(((xb-xn)/xb)*100);
+          n=n+1;
+          fprintf('\nIter. \t Xn \t\t ERROR AB. \n');
+          fprintf('%2i \t %f \t  %f\n', n, xn, er);
+          xb=xn;
+       else
+            n=n+1;
+            xb=xn;
+       endif  
+      endwhile
       ## SALIDA
          fprintf('\n\n================================');
          fprintf('\n   TOLERANCIA: %f',tol);
          fprintf('\n APROXIMACIÓN: %f', xn);
-         fprintf('\n  ITERACIONES: %i', ciclo);
+         fprintf('\n  ITERACIONES: %i', n);
          fprintf('\n\n\n');
-  
-      
-  
-  
   endfunction
