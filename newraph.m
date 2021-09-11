124d5e09fc5d727a396232b689c7e6eeee20a299
Kevin Brennan <82340612+Kevin-B-UTP@users.noreply.github.com>
2021-09-11 17:55:33 -0500

    Update newraph.m

--- newraph.m
+++ newraph.m
@@ -2,34 +2,32 @@
 % Grupo: Kevin Brennan, Edwar Gonzalez, Guillermo Lezcano
 % Escrito por Kevin Brennan - 2021
 
-## newtonraphson(función, aproximacion=a, 0.0001)
+## newtonraphson(función, derivada de la funcion, aproximacion=a, 0.0001)
 
-function xn = newtonraphson (funcion, a, tol)
+function xn = newtonraphson (f, df, a, tol)
       clc;
-      error = 10000;
+      er = 10000;
       n=0;
       ## ENCABEZADO
       fprintf('\n__________________________________');
       fprintf('\n   MÉTODO DE LA NEWTON-RAPHSON');
       fprintf('\n__________________________________');
-      ## INSTRUCCIONES
-      
-      f = inline(funcion);
-      derivada_funcion = diff(funcion, x);
-      df = inline (derivada_funcion);
+         
       ## CICLO- EVALUACIÓN- APROXIMACIÓN ?
       while (er>tol) 
-      fa = feval (f, a);
-      dfa = feval (df, a);
+      f = feval (f, a);
+      df = feval (df, a);
       xn = a - ((fa)/(dfa));
         if (n>0)
-        
-          er=abs(((xb-xn)/xb)*100);
+          er=abs(((xb-xn)/xb))*100;
           n=n+1;
           fprintf('\nIter. \t Xn \t\t ERROR AB. \n');
           fprintf('%2i \t %f \t  %f\n', n, xn, er);
           xb=xn;
-       else
+       endif    
+       if (n=0)
+            fprintf('\nIter. \t Xn \n');
+            fprintf('%2i \t %f \t  %f\n', n, xn);
             n=n+1;
             xb=xn;
        endif  
