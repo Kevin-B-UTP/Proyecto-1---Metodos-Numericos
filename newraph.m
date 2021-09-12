509bbc2083f98d511f501cc30a972f5d1917db83
Kevin Brennan <82340612+Kevin-B-UTP@users.noreply.github.com>
2021-09-11 22:44:39 -0500

    Update newraph.m

--- newraph.m
+++ newraph.m
@@ -2,40 +2,32 @@
 % Grupo: Kevin Brennan, Edwar Gonzalez, Guillermo Lezcano
 % Escrito por Kevin Brennan - 2021
 
-## newtonraphson(función, derivada de la funcion, aproximacion=a, 0.0001)
+## newtonraphson(función, derivada de la funcion, aproximacion, 0.0001)
 
 function xn = newtonraphson (f, df, a, tol)
-      clc;
-      er = 10000;
-      n=0;
-      ## ENCABEZADO
-      fprintf('\n__________________________________');
-      fprintf('\n   MÉTODO DE LA NEWTON-RAPHSON');
-      fprintf('\n__________________________________');
-      f = feval (f, a);
-      df = feval (df, a);
-      ## CICLO- EVALUACIÓN- APROXIMACIÓN
-      while (er>tol) 
-        if (n>0)
-          xn = a - ((f)/(df));
-          er= abs(((xn-xb)/xn)*100);
-          fprintf('\nIter. \t Xn \t\t ERROR. \n');
-          fprintf('%2i \t %f \t  %f\n', n, xn, er);
-          n=n+1;
-          a=xn;
-       endif    
-         if (n==0)
-              xn = a - ((f)/(df));
-              fprintf('\nIter. \t Xn \n');
-              fprintf('%2i \t %f \t  %f\n', n, a);
-              n=n+1;
+clc;
+er = 10000;
+n=0;
+
+## ENCABEZADO
+    fprintf('\n__________________________________');
+    fprintf('\n   MÉTODO DE LA NEWTON-RAPHSON');
+    fprintf('\n__________________________________');  
+## CICLO- EVALUACIÓN- APROXIMACIÓN
+    while (er>tol) 
+      xn = a - ((feval (f, a)))/((feval (df, a)));
+         if (n!=0)
+             er=  abs(xn - xb);
          endif
-       xb=xn;
-      endwhile
-      ## SALIDA
-         fprintf('\n\n===================================');
+      fprintf('\nIter. \t Xn \t\t ERROR. \n');
+      fprintf('%2i \t %f \t  %f\n', n, xn, er);
+      n=n+1;
+      xb = xn;  
+    endwhile
+## SALIDA
+fprintf('\n\n================================');
          fprintf('\n   TOLERANCIA: %f',tol);
          fprintf('\n APROXIMACIÓN: %f', xn);
          fprintf('\n  ITERACIONES: %i', n);
-         fprintf('\n\n\n');
-  endfunction
+         fprintf('\n\n\n');   
+endfunction
