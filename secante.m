ef96e396edec8cffdf36c1a5a8f39362a5c71665
Kevin Brennan <82340612+Kevin-B-UTP@users.noreply.github.com>
2021-09-11 23:44:12 -0500

    Update secante.m

--- secante.m
+++ secante.m
@@ -11,11 +11,11 @@
   error = 100;
   n = 0;
   fprintf('    n     xo       x1      x2    error\n');
-  fprintf('    %i    %4.4f  %4.4f  ----  ----\n',n,x0,x1);
+  fprintf('%i \t %4.4f \t %4.4f \t ---- \t ----\n',n,x0,x1);
   while ( error>tol )
      x2 = x1 - (x1-x0)*f(x1)/(f(x1)-f(x0));
      error = abs ( f(x2) );
-     fprintf('    %i   %4.4f    %4.4f  %4.4f \n',n,x0,x1,x2,error);
+     fprintf('%i  \t %4.4f  \t   %4.4f \t %4.4f \n',n,x0,x1,x2,error);
      x0 = x1;
      x1 = x2;
      n = n+1;   
